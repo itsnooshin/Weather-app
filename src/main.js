c7fd6eaaf5c73829eec46fe715978c7c07bce3ee
Nooshin bakhtiari <nooshindev@gmail.com>
2023-06-25 23:34:16 +0330

    delete the extra comments on javascript file

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ const cloundyEl = document.querySelector('.cloud-num');
 const windEl = document.querySelector('.wind-num');
 const rainEl = document.querySelector('.rain-num');
 
-// <span class="clock-hour">12</span>:<span class="clock-minute">30</span> - <span class="clock-day">Thursday </span><span class="clock-date">22</span>, <span class="clock-month">06</span>  <span class="clock-year">2021</span>
-
 searchWeather.addEventListener('click', function () {
   let apikey = 'da13c92adcb97e26e489d8a4eccc88b9';
   let monthNames = [
@@ -95,37 +93,3 @@ searchWeather.addEventListener('click', function () {
 
   weather();
 });
-
-// const weather = async function () {
-//   let apikey = 'da13c92adcb97e26e489d8a4eccc88b9';
-//   let city = 'tehran';
-
-//   const response = await fetch(
-//     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
-//   );
-//   if (response.ok) {
-//     const data = await response.json();
-//     const lat = data.coord.lat;
-//     const lon = data.coord.lon;
-//     const temperature = Math.ceil((data.main.temp - 273.15).toFixed(2));
-//     // console.log(temperature);
-//     // console.log(data);
-//     // console.log(data.main);
-//     const [cloudyPer] = Object.values(data.clouds);
-//     // console.log(data);
-//     getAllweather(lat, lon);
-//   }
-// };
-// weather();
-// // for next days
-// const getAllweather = async function (lat, lon) {
-//   const res = await fetch(
-//     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=da13c92adcb97e26e489d8a4eccc88b9`
-//   );
-//   const data = await res.json();
-//   // console.log(data.list);
-//   const [list] = data.list;
-//   // console.log(data);
-//   const elmentOfpop = list.pop;
-//   // console.log(elmentOfpop);
-// };
