0824a1a120f22f9f1f2de16bde36ff9e274cd172
Nooshin bakhtiari <nooshindev@gmail.com>
2023-06-30 16:57:31 +0330

    add weather to next 5 days and display it

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ const windEl = document.querySelector('.wind-num');
 const rainEl = document.querySelector('.rain-num');
 const days = document.querySelectorAll('.day');
 const citiesPrev = document.querySelectorAll('.title__cities');
+const maxTemp = document.querySelectorAll('.tem__max');
+const minTemp = document.querySelectorAll('.tem__min');
+const week = document.querySelector('.weather__week-info');
+const imges = document.querySelectorAll('.img__weather-week');
 
 let monthNames = [
   'January',
@@ -34,10 +38,11 @@ let dayNames = [
   'Friday',
   'saturday',
 ];
-
+const dayWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'sat'];
+// https://api.openweathermap.org/data/2.5/onecall?lat=tehran&lon=${cityLon}&exclude=minutely,hourly,current&units=metric&appid=da13c92adcb97e26e489d8a4eccc88b9
 async function weather() {
   let apikey = 'da13c92adcb97e26e489d8a4eccc88b9';
-  let city = 'New York';
+  let city = 'Tehran';
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
   );
@@ -141,36 +146,36 @@ async function weather() {
     }
     getAllweather(lat, lon);
   }
-
-  // console.log(data); // data for weather now
-  //  snow , rain , Drizzle , Thunderstorm , Clear	, Clouds
 }
 
 weather();
 
 async function getAllweather(lat, lon) {
+  let YOUR_API_KEY = 'da13c92adcb97e26e489d8a4eccc88b9';
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=da13c92adcb97e26e489d8a4eccc88b9&units=metric`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${YOUR_API_KEY}&units=metric`
   );
   const data = await res.json();
-
+  console.log(data);
   const forecastList = data.list;
   const currentDate = new Date();
   // Filter the forecast data for the next 5 days
   const nextDays = forecastList.filter(item => {
     const itemDate = new Date(item.dt * 1000);
     let el = new Date(itemDate);
-
+    // console.log(el);
+    //  console.log(el);
     if (itemDate.getDate() !== currentDate.getDate()) {
       const day = new Date(itemDate);
+      // console.log(itemDate.getDate(), currentDate.getDate());
       return itemDate.getDate();
     }
   });
-  
+
   nextDays.forEach(item => {
     const itemdate = new Date(item.dt * 1000);
+
     const newdel = itemdate.toString().slice(0, 15);
-    // console.log(newdel, item.main.temp);
   });
 
   const obj = {};
@@ -180,60 +185,62 @@ async function getAllweather(lat, lon) {
       // console.log(item.weather);
       const itemDate = new Date(item.dt * 1000);
       const newdel = itemDate.toString().slice(0, 15);
+      let temp = item.main.temp;
+
       if (itemDate.getDate() !== currentDate.getDate()) {
-    
-        let temp = item.main.temp;
-        // console.log(temp);
+        let weather = item.weather[0];
 
         if (!obj[newdel]) {
           obj[newdel] = {
             min: temp,
             max: temp,
+            image: weather,
           };
         } else {
-          obj[newdel].min = Math.min(obj[newdel].min, temp);
+          obj[newdel].min = Math.round(Math.min(obj[newdel].min, temp));
           // console.log(temp, itemDate);
-          obj[newdel].max = Math.max(obj[newdel].max, temp);
+          obj[newdel].max = Math.round(Math.max(obj[newdel].max, temp));
+          obj[newdel].image = weather;
         }
-
         return itemDate.getDate();
       }
     });
   }
-  // console.log(obj);
-  for (const key in obj) {
-    const { min, max } = obj[key];
-    // console.log(key, min, max);
+
+  console.log(obj);
+  const element = Object.entries(obj);
+
+  for (let i = 0; i < element.length; i++) {
+    const el = element[i];
+    const day = days[i];
+    const newel = new Date(el[0]);
+    const option = { weekday: 'short' };
+    const wwkOfday = newel.toLocaleDateString('en-US', option);
+    const mint = minTemp[i];
+    const maxt = maxTemp[i];
+    mint.textContent = el[1].min;
+    maxt.textContent = el[1].max;
+    day.textContent = wwkOfday;
+    // console.log(el, el[1].image);
+    // console.log(el[1].image.main);
+    // console.log(imges[i].src);
+    if (el[1].image.main === 'Clear') {
+      imges[i].src = '/clear.629910ed.png';
+    }
+    if (el[1].image.main === 'Clouds') {
+      imges[i].src = '/cloudy-weather.ea7810a0.png';
+    }
+    if (el[1].image.main === 'Rain') {
+      console.log(imges[i]);
+      imges[i].src = '/rain-iconpng.0d69055a.png';
+    }
+
+    // 'Clouds' , 'Rain' , 'Rain' , Clear
   }
 }
 
 searchWeather.addEventListener('click', function () {
   let apikey = 'da13c92adcb97e26e489d8a4eccc88b9';
-  let monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-
-  const dayNames = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'saturday',
-  ];
-  const dayWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'sat'];
   const cityel = inputCityWeather.value;
   const el2 = cityel
     .split(' ')
@@ -315,10 +322,7 @@ searchWeather.addEventListener('click', function () {
       weatherEl.main === 'Mist' ||
       weatherEl.main === 'Smoke' ||
       weatherEl.main === 'Haze'
-      // weatherEl.id >= 701 &&
-      // weatherEl.id <= 781
     ) {
-      console.log('hh');
       const img = document.querySelector('.img__weather');
       img.src = '/iconizer-weather.f6570355.svg';
       document.body.style.backgroundImage = 'url(/foggy-forest-4.6d264000.jpg)';
@@ -365,29 +369,10 @@ searchWeather.addEventListener('click', function () {
       const date = new Date(data1);
       const day = date.getDay();
       const weeek = dayWeek[day];
-      // console.log(weeek);
-      // days.forEach(day => {
-      //   // day.textContent = weeek;
-      //   // console.log(day.textContent = weeek);
-      // console.log(list);
-
-      // // });
-      // console.log(list);
     });
 
     const elmentOfpop = list.pop;
-    // rainEl.textContent = elmentOfpop;
-    // const listaRR = data.list;
-    // // console.log(listaRR.filter(el => console.log(el.main.temp)));
-    // console.log(data);
-    // console.log(object.value(list));
   }
 
   weather();
 });
-
-// citiesPrev.forEach(function (btn) {
-//   btn.addEventListener('click', function () {
-//     console.log(btn.textContent);
-//   });
-// });
